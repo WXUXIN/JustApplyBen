3a7f7cde60756a74a6066d36bc9553cfa4769c7c
ben <wangxuxin1@gmail.com>
2025-09-07 02:08:03 +0800

    confict

--- daily_scraper.py
+++ daily_scraper.py
@@ -246,6 +246,8 @@ def main():
         "https://www.quantedge.com/careers",
         "https://careers.twosigma.com/careers/OpenRoles/?5081=%5B16718737%5D&5081_format=3146&listFilterMode=1&jobRecordsPerPage=10&",
         "https://careers.point72.com/?experience=internships&location=singapore;hong%20kong;sydney",
+        "https://lifeattiktok.com/referral/tiktok/campus/?keywords=Engineer&category=&location=CT_163&project=7459986622530078983%2C7459987887569733896&type=&job_hot_flag=&current=1&limit=10&functionCategory",
+        "https://joinbytedance.com/search?job_category_id_list=&location_code_list=CT_163&recruitment_id_list=202%2C301%2C201&subject_id_list=&tag_id_list=&keyword=Engineer&limit=12&offset=0",
         "https://jobs.lever.co/palantir?commitment=Internship",
         "https://bloomberg.avature.net/careers/SearchJobs/?1845=%5B162487%5D&1845_format=3996&2562=%5B219290%5D&2562_format=6594&listFilterMode=1&jobRecordsPerPage=12&"
     ]
