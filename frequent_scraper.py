7ea4ac59c9f390583537b45623b5088da8a17e19
ben <wangxuxin1@gmail.com>
2025-09-08 11:26:22 +0800

    added linkedin 24hrs

--- frequent_scraper.py
+++ frequent_scraper.py
@@ -238,6 +238,7 @@ def main():
     urls = [
         "https://lifeattiktok.com/referral/tiktok/campus/?keywords=Engineer&category=&location=CT_163&project=7459986622530078983%2C7459987887569733896&type=&job_hot_flag=&current=1&limit=10&functionCategory",
         "https://joinbytedance.com/search?job_category_id_list=&location_code_list=CT_163&recruitment_id_list=202%2C301%2C201&subject_id_list=&tag_id_list=&keyword=Engineer&limit=12&offset=0",
+        "https://www.linkedin.com/jobs/search-results/?distance=50.0&f_TPR=r3600&geoId=103804675&keywords=software%20engineer&origin=JOBS_HOME_KEYWORD_HISTORY" 
     ]
 
     process_urls(urls)
